1c08e884f160f1c56200e852a175eecb2dec4e48
ritu <ritu.e.kumari@capgemini.com>
2024-06-17 11:29:05 +0530

    spring security code

---
+++ src/main/java/com/demo/repository/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package com.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.demo.entity.UserInfo;
+import java.util.List;
+import java.util.Optional;
+
+
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer>{
+	Optional<UserInfo> findByName(String name);
+}
